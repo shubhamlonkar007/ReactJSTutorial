6df474fbff51fb2d19b4b81dfed80845037431f9
shubham <shubhamlonkar2294@gmail.com>
2020-06-20 22:21:41 +0530

    updated code so far

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import Person from "./Person/Person";
 
-// import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
 
 /* function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
@@ -16,12 +16,12 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      { id: '1', name: 'Shubham', age: 24 },
-      { id: '2', name: 'Patrick', age: 45 },
-      { id: '3', name: 'Tony', age: 54 },
+      { id: "1", name: "Shubham", age: 24 },
+      { id: "2", name: "Patrick", age: 45 },
+      { id: "3", name: "Tony", age: 54 }
     ],
-    otherState: 'some other value',
-    showPersons: false,
+    otherState: "some other value",
+    showPersons: false
   };
 
   /* switchNameHandler = (newName) => {
@@ -34,45 +34,45 @@ class App extends Component {
     })
   } */
 
-  handlerNameChanged = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      console.log('p.id==>', p.id);
-      console.log('id===>', id);
-      console.log('p.id===id===>', p.id === id);
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      console.log("p.id==>", p.id);
+      console.log("id===>", id);
+      console.log("p.id===id===>", p.id === id);
       return p.id === id;
     });
 
-    console.log('personIndex===>', personIndex);
+    console.log("personIndex===>", personIndex);
 
     const person = {
-      ...this.state.persons[personIndex],
+      ...this.state.persons[personIndex]
     };
 
-    console.log('person===>', person);
+    console.log("person===>", person);
 
     person.name = event.target.value;
 
-    console.log('person.name==>', person.name);
+    console.log("person.name==>", person.name);
 
     const persons = [...this.state.persons];
 
-    console.log('persons====>', persons);
+    console.log("persons====>", persons);
 
     persons[personIndex] = person;
 
     this.setState({ persons: persons });
   };
 
-  handleTogglePersons = () => {
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    console.log('doesShow===>', doesShow);
+    console.log("doesShow===>", doesShow);
     this.setState({
-      showPersons: !doesShow,
+      showPersons: !doesShow
     });
   };
 
-  handleDeletePerson = (personIndex) => {
-    // const persons = this.state.persons;
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.persons;
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
@@ -80,12 +80,11 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
     };
 
     let persons = null;
@@ -96,37 +95,23 @@ class App extends Component {
           {this.state.persons.map((person, index) => {
             return (
               <Person
-                handleClick={() => this.handleDeletePerson(index)}
+                click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
                 key={person.id}
-                handleChanged={(event) =>
-                  this.handleNameChanged(event, person.id)
-                }
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
         </div>
       );
-      style.backgroundColor = 'red';
     }
-
-    let classes = [];
-    if (this.state.persons.length <= 2) {
-      classes.push('red');
-    }
-
-    if(this.state.persons.length <= 1) {
-      classes.push('bold');
-    }
-
-
 
     return (
-      <div className='App'>
+      <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.handleTogglePersons}>
+        <p>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
           Switch Name
         </button>
         {persons}
@@ -134,5 +119,6 @@ class App extends Component {
     );
   }
 }
+
 
 export default App;
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './Person.css'
+import React from "react";
+import "./Person.css";
 const Person = props => {
   return (
-    <div className='Person'>
-      <p onClick={props.handleClick}>
+    <div className="Person">
+      <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} years old!
       </p>
       <p>{props.children}</p>
-      <input type='text' onChange={props.handleChanged} value={props.name} />
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
-}
+};
 
-export default Person
+export default Person;
