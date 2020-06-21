0d181012ec6d0baa3ec099094c00e2e32b7a3623
shubham <shubhamlonkar2294@gmail.com>
2020-06-21 21:54:15 +0530

    Burger builder application code upto PropTypes and passing state data from Burger component

---
+++ burger-builder/src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+     const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        
+         console.log("igKey",igKey);
+         return [...Array(props.ingredients[igKey])].map((_, i) => {
+            console.log("i",i);
+             return <BurgerIngredient  key={ igKey + i} type={igKey} />
+         });
+     });
+
+    
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type='bread-top' />
+      {transformedIngredients}
+      <BurgerIngredient type='bread-bottom' />
+    </div>
+  );
+};
+
+export default burger;
